469f8383eefb3525deae3aec0acf1d156f1757e9
Andreas Gassmann <andreas@andreasgassmann.ch>
2023-08-18 12:59:32 +0200

    feat(): update header

--- src/app/header-item/header-item.component.ts
+++ src/app/header-item/header-item.component.ts
@@ -3,6 +3,7 @@ import { BsModalService } from 'ngx-bootstrap/modal';
 import { first } from 'rxjs/operators';
 import { ConfirmModalComponent } from '../components/confirm-modal/confirm-modal.component';
 import { MetamaskService } from '../services/metamask.service';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-header-item',
@@ -10,7 +11,10 @@ import { MetamaskService } from '../services/metamask.service';
   styleUrls: ['./header-item.component.scss'],
 })
 export class HeaderItemComponent implements OnInit {
-  constructor(public readonly metamaskService: MetamaskService) {}
+  constructor(
+    public readonly metamaskService: MetamaskService,
+    public readonly accountService: AccountService
+  ) {}
 
   ngOnInit(): void {}
 }
