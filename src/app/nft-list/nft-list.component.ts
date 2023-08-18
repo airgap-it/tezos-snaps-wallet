c2ce21f48b3e1819e14ea9009e27f92a3039c918
Andreas Gassmann <andreas@andreasgassmann.ch>
2023-08-18 13:00:16 +0200

    feat(): add nft component

---
+++ src/app/nft-list/nft-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Token } from '../types';
+
+@Component({
+  selector: 'app-nft-list',
+  templateUrl: './nft-list.component.html',
+  styleUrls: ['./nft-list.component.scss'],
+})
+export class NftListComponent implements OnInit {
+  @Input() nfts: Token[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
