4702af0689c91aaacfc76aa5c1d59c42f343b615
Andreas Gassmann <andreas@andreasgassmann.ch>
2023-08-29 13:50:48 +0200

    feat(): account component

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { SendTezModalComponent } from 'src/app/send-tez-modal/send-tez-modal.component';
 import { Token } from 'src/app/types';
 
 @Component({
@@ -12,7 +14,25 @@ export class AccountComponent implements OnInit {
   @Input() usdBalance: string = '';
   @Input() tokens: Token[] = [];
 
-  constructor() {}
+  constructor(public readonly modalService: BsModalService) {}
 
   ngOnInit(): void {}
+
+  openLink(link: string) {
+    window.open(link, '_blank');
+  }
+
+  sendTez() {
+    const initialState: ModalOptions<SendTezModalComponent> = {
+      initialState: {
+        recipient: 'tz1',
+        amount: '123',
+      },
+      class: 'modal-dialog-centered',
+    };
+    const bsModalRef = this.modalService.show(
+      SendTezModalComponent,
+      initialState
+    );
+  }
 }
