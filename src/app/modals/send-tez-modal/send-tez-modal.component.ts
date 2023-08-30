e36ebef2762ab5170218c850d532525aa4df74bc
Andreas Gassmann <andreas@andreasgassmann.ch>
2023-08-30 16:23:57 +0200

    feat(): add send tez modal functionality

--- src/app/modals/send-tez-modal/send-tez-modal.component.ts
+++ src/app/modals/send-tez-modal/send-tez-modal.component.ts
@@ -1,8 +1,10 @@
 import { TezosOperationType } from '@airgap/beacon-wallet';
 import { Component, OnInit } from '@angular/core';
+import BigNumber from 'bignumber.js';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { first } from 'rxjs/operators';
 import { AccountService } from 'src/app/services/account.service';
+import { ApiService } from 'src/app/services/api.service';
 import { BeaconService } from 'src/app/services/beacon.service';
 import { sendOperationRequest } from 'src/app/utils/snap';
 
@@ -14,12 +16,17 @@ import { sendOperationRequest } from 'src/app/utils/snap';
 export class SendTezModalComponent implements OnInit {
   recipient: string = '';
   amount: string = '';
+  usdAmount: string = '0.00';
+  usdPrice: number = 0;
 
   constructor(
     public readonly bsModalRef: BsModalRef,
+    public readonly apiService: ApiService,
     public readonly accountService: AccountService,
     public readonly beaconService: BeaconService
-  ) {}
+  ) {
+    this.apiService.getXtzPrice().then((price) => (this.usdPrice = price));
+  }
 
   ngOnInit(): void {}
 
@@ -34,5 +41,10 @@ export class SendTezModalComponent implements OnInit {
 
     console.log('RESULT', result);
     this.bsModalRef.hide();
+  }
+
+  onAmountChanged(amount: string) {
+    this.amount = amount;
+    this.usdAmount = new BigNumber(amount).times(this.usdPrice).toString(10);
   }
 }
