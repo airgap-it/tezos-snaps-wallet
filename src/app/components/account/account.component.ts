d2145d5c6c6fda5ed3e053a08a8351fa0cd70a94
Andreas Gassmann <andreas@andreasgassmann.ch>
2023-09-06 15:52:33 +0200

    feat(): tez balance styling

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  Input,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Token } from '../../types';
 import { ModalService } from 'src/app/services/modal.service';
 
